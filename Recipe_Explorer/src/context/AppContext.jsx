eb3db6f392706f1c2934659264785cc6d8467a32
abhirup2565 <abhirup2565@gmail.com>
2025-10-28 21:55:12 +0530

    added context_API

---
+++ Recipe_Explorer/src/context/AppContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+  
+
+  return (
+    <AppContext.Provider value={{
+      favorites,setFavorites,
+      cart,setCart,
+      loading,setLoading
+    }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useApp = () => useContext(AppContext);
